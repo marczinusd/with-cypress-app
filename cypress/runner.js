6b306843424e47027532af73439b04703c19a475
David Marczinus <marczinus.david@gmail.com>
2023-11-09 16:57:23 +0100

    Create runner.js

---
+++ cypress/runner.js
@@ -0,0 +1,28 @@
+const cypress = require("cypress");
+const tesults = require("cypress-tesults-reporter");
+
+const token = process.env.TESULTS_TOKEN;
+if (token === undefined) {
+  throw new Error(
+    "Tesults token was not found -- please set the TESULTS_TOKEN environment variable."
+  );
+}
+
+cypress
+  .run({
+    reporter: "junit",
+    browser: "electron",
+    config: {
+      baseUrl: "http://localhost:3000",
+      video: true,
+    },
+  })
+  .then((results) => {
+    const args = {
+      target: token,
+    };
+    tesults.results(results, args);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
