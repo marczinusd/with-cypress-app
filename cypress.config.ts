601e012bbf43b51122830b8dc89a55ad5eca43f1
David Marczinus <marczinus.david@gmail.com>
2023-11-09 18:07:08 +0100

    added allure support to cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,23 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress";
+import allureWriter from "@shelex/cypress-allure-plugin/writer";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: "http://localhost:3000",
+    setupNodeEvents(on, config) {
+      allureWriter(on, config);
+      return config;
+    },
+    env: {
+      allure: true,
+      allureReuseAfterSpec: true,
+      allureAddVideoOnPass: true,
+    },
   },
   component: {
     devServer: {
-      framework: 'next',
-      bundler: 'webpack',
+      framework: "next",
+      bundler: "webpack",
     },
   },
-})
+});
